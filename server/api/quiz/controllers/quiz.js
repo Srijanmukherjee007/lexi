3a3c28eccd580daf2ed81d85ed1bd3ab51e9f959
Srijanmukherjee007 <mukherjeesrijan2@gmail.com>
2021-06-21 11:40:57 +0530

    updated server, added randomquestion routes and graphql support

--- server/api/quiz/controllers/quiz.js
+++ server/api/quiz/controllers/quiz.js
@@ -1,8 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async randomquestion(ctx) {
+    const quiz = await strapi.query("quiz").findOne({ _id: ctx.params.id });
+    if (!quiz) {
+      ctx.send(
+        {
+          error: "quiz does not exist",
+        },
+        404
+      );
+
+      return;
+    }
+
+    const questions = quiz.questions;
+
+    if (questions.length == 0) {
+      ctx.send({
+        error: "No questions available",
+      });
+
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const { question_text: question, options } = questions[randomIndex];
+    ctx.send({
+      question,
+      options,
+    });
+    return;
+  },
+};
