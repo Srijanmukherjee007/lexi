7ae3a47dc13cb1253720cc7e91e67d4defd97536
Srijanmukherjee007 <mukherjeesrijan2@gmail.com>
2021-06-22 14:43:56 +0530

    added endpoint /quiz/slug/<slug>/randomquestion

--- server/api/quiz/controllers/quiz.js
+++ server/api/quiz/controllers/quiz.js
@@ -37,4 +37,36 @@ module.exports = {
     });
     return;
   },
+
+  async randomquestionslug(ctx) {
+    const quiz = await strapi.query("quiz").findOne({ slug: ctx.params.slug });
+    if (!quiz) {
+      ctx.send(
+        {
+          error: "quiz does not exist",
+        },
+        404
+      );
+
+      return;
+    }
+
+    const questions = quiz.questions;
+
+    if (questions.length == 0) {
+      ctx.send({
+        error: "No questions available",
+      });
+
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const { question_text: question, options } = questions[randomIndex];
+    ctx.send({
+      question,
+      options,
+    });
+    return;
+  },
 };
